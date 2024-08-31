0b3db647871655e749da2bf49eb883490f3c6a55
bhumimittal31 <128024511+bhumimittal31@users.noreply.github.com>
2024-08-31 16:22:40 +0530

    Resolve white page issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {BrowserRouter , Routes, Route} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename ="/font_learning/">
+      <Routes>
+        <Route path = "*" element={<App/>}/>
+      </Routes>
+    </BrowserRouter>
+    // <App />
   </StrictMode>,
 )
